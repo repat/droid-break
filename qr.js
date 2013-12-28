b64375f76d85f43b10b0d30bd5d1917e3cb9df42
LumenTeun <teun@lutic.org>
2013-12-28 17:12:33 +0100

    Replaced yesterdays incredibly dirty and disfunctional code with slitghly cleaner and more functional code. Slightly.

--- qr.js
+++ qr.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.qr').click(function(){
         qrName = $(this).attr('id');
-        $('body').prepend('<div id="qrFade"></div><div id="qrBackground"><div id="qrText"><h1>'+qrName+'</h1><h4>Scan using a barcode scanner.</h4></div><img src="'+qrName+'.gif" alt="'+qrName+' QR" id="qrCode" /></div>');
+        $('body').prepend('<div id="qrFade"></div><div class="emph-Block" id="qrBackground"><div id="qrText"><h2>'+qrName+'</h2><h4>Scan using a barcode scanner.</h4></div><img src="'+qrName+'.gif" alt="'+qrName+' QR" id="qrCode" /></div>');
         $('#qrFade').fadeIn('slow');
         $('#qrBackground').fadeIn('slow');
         $('#qrText').fadeIn('slow');
